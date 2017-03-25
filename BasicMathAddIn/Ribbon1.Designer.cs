e6c43d9b5493b0a9a024ec6d680db6e01e573aca
Why7090 <whyharry9@gmail.com>
2017-03-25 10:33:07 -0400

    添加项目文件。

---
+++ BasicMathAddIn/Ribbon1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace BasicMathAddIn
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1 ()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon1));
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl1 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl2 = this.Factory.CreateRibbonDropDownItem();
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.editBox2 = this.Factory.CreateRibbonEditBox();
+            this.checkBox1 = this.Factory.CreateRibbonCheckBox();
+            this.checkBox2 = this.Factory.CreateRibbonCheckBox();
+            this.dropDown1 = this.Factory.CreateRibbonDropDown();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.editBox1 = this.Factory.CreateRibbonEditBox();
+            this.editBox3 = this.Factory.CreateRibbonEditBox();
+            this.group3 = this.Factory.CreateRibbonGroup();
+            this.editBox4 = this.Factory.CreateRibbonEditBox();
+            this.editBox5 = this.Factory.CreateRibbonEditBox();
+            this.tab1.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.group3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group2);
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.group3);
+            resources.ApplyResources(this.tab1, "tab1");
+            this.tab1.Name = "tab1";
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.button1);
+            this.group2.Items.Add(this.editBox2);
+            this.group2.Items.Add(this.checkBox1);
+            this.group2.Items.Add(this.checkBox2);
+            this.group2.Items.Add(this.dropDown1);
+            resources.ApplyResources(this.group2, "group2");
+            this.group2.Name = "group2";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = global::BasicMathAddIn.Properties.Resources.Dice_80px;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // editBox2
+            // 
+            resources.ApplyResources(this.editBox2, "editBox2");
+            this.editBox2.Name = "editBox2";
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Name = "checkBox2";
+            // 
+            // dropDown1
+            // 
+            resources.ApplyResources(ribbonDropDownItemImpl1, "ribbonDropDownItemImpl1");
+            resources.ApplyResources(ribbonDropDownItemImpl2, "ribbonDropDownItemImpl2");
+            this.dropDown1.Items.Add(ribbonDropDownItemImpl1);
+            this.dropDown1.Items.Add(ribbonDropDownItemImpl2);
+            resources.ApplyResources(this.dropDown1, "dropDown1");
+            this.dropDown1.Name = "dropDown1";
+            this.dropDown1.ShowItemImage = false;
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.editBox1);
+            this.group1.Items.Add(this.editBox3);
+            resources.ApplyResources(this.group1, "group1");
+            this.group1.Name = "group1";
+            // 
+            // editBox1
+            // 
+            resources.ApplyResources(this.editBox1, "editBox1");
+            this.editBox1.Name = "editBox1";
+            // 
+            // editBox3
+            // 
+            resources.ApplyResources(this.editBox3, "editBox3");
+            this.editBox3.Name = "editBox3";
+            // 
+            // group3
+            // 
+            this.group3.Items.Add(this.editBox4);
+            this.group3.Items.Add(this.editBox5);
+            resources.ApplyResources(this.group3, "group3");
+            this.group3.Name = "group3";
+            // 
+            // editBox4
+            // 
+            resources.ApplyResources(this.editBox4, "editBox4");
+            this.editBox4.Name = "editBox4";
+            // 
+            // editBox5
+            // 
+            resources.ApplyResources(this.editBox5, "editBox5");
+            this.editBox5.Name = "editBox5";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.group3.ResumeLayout(false);
+            this.group3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox checkBox1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox checkBox2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDown1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ BasicMathAddIn/Ribbon1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace BasicMathAddIn
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load (object sender, RibbonUIEventArgs e)
+        {
+        }
+
+        private void button1_Click (object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                var doc = Globals.ThisAddIn.Application.ActiveDocument;
+                doc.Content.Font.NameAscii = "Arial";
+                doc.Content.Font.Size = 28;
+                doc.PageSetup.TextColumns.SetCount(2);
+
+                bool add = checkBox1.Checked;
+                bool sub = checkBox2.Checked;
+                if (!(add || sub))
+                {
+                    return;
+                }
+                int pages = Convert.ToInt16(editBox2.Text);
+                int length = 30 * pages;
+                int maxA = 1 + Convert.ToInt16(editBox1.Text);
+                int minA = Math.Min(Convert.ToInt16(editBox3.Text), maxA);
+                int maxS = 1 + Convert.ToInt16(editBox4.Text);
+                int minS = Math.Min(Convert.ToInt16(editBox5.Text), maxS);
+                var lst = new List<string>();
+                var rand = new Random();
+
+                for (int i = 0; i < length; i++)
+                {
+                    int type;
+                    if (add && sub)
+                    {
+                        type = rand.Next(0, 2);
+                    }
+                    else
+                    {
+                        type = add ? 0 : 1;
+                    }
+
+                    if (type == 0)
+                    {
+                        int result = rand.Next(minA, maxA);
+                        int val1 = rand.Next(result + 1);
+                        int val2 = result - val1;
+                        lst.Add(string.Format("{0} + {1} = ", val1, val2));
+                    }
+                    else
+                    {
+                        int val1 = rand.Next(minS, maxS);
+                        int val2 = rand.Next(val1 + 1);
+                        lst.Add(string.Format("{0} - {1} = ", val1, val2));
+                    }
+                }
+                string s = string.Join("\n", lst);
+                if (dropDown1.SelectedItem.Label == "覆盖")
+                {
+                    doc.Content.Text = s;
+                }
+                else
+                {
+                    doc.Content.Text += s;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
